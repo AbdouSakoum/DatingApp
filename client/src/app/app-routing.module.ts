edf82a0ba9c570afd1603c1ffbed040b183afeab
AbdouSakoum <abd1111sakoum@gmail.com>
2023-08-31 23:28:06 +0100

    end section 7

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
+import { TestErrorComponent } from './errors/test-error/test-error.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 // import { authGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
@@ -17,11 +20,13 @@ const routes: Routes = [
       {path: 'members', component: MemberListComponent},
       {path: 'members/:id', component: MemberDetailComponent},
       {path: 'lists', component: ListsComponent},
-      {path: 'messages', component: MessagesComponent}
+      {path: 'messages', component: MessagesComponent}, 
     ]
   },
-  {path: '**', component: HomeComponent, pathMatch: 'full'},
-];
+  {path: 'errors', component: TestErrorComponent},
+  {path: 'not-found', component: NotFoundComponent},
+  {path: 'server-error', component: ServerErrorComponent},
+  {path: '**', component: NotFoundComponent, pathMatch: 'full'},   ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
